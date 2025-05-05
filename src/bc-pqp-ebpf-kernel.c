167b8513f978eee27ae257487979f29b4af8cca8
Friedrich Tschirpke <friedrich.tschirpke@live.de>
2025-05-05 21:09:51 +0200

    feat: finished echo xdp program

--- src/bc-pqp-ebpf-kernel.c
+++ src/bc-pqp-ebpf-kernel.c
@@ -1,107 +1,86 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include <linux/bpf.h>
-#include <linux/in.h>
+#include <linux/if_ether.h>
 #include <linux/types.h>
 
 #include <bpf/bpf_helpers.h>
+#include <bpf/bpf_endian.h>
 #include <xdp/parsing_helpers.h>
 
-// the original function from <xdp/parsing_helpers.h> copied for debugging purposes
-static __always_inline int parse_ethhdr_own(struct hdr_cursor *nh, void *data_end,
-					struct ethhdr **ethhdr)
-{
-	struct ethhdr *eth = nh->pos;
-	struct vlan_hdr *vlh;
-	__u16 h_proto;
-
-	if (eth + 1 > data_end)
-		return -1;
-
-	nh->pos = eth + 1;
-	*ethhdr = eth;
-	vlh = nh->pos;
-	h_proto = eth->h_proto;
-
-	/* Use loop unrolling to avoid the verifier restriction on loops;
-	 * support up to VLAN_MAX_DEPTH layers of VLAN encapsulation.
-	 */
-    // BUG: loop causes errors on loading
-	/* #pragma unroll */
-	/* for (int i = 0; i < VLAN_MAX_DEPTH; i++) { */
-	/* 	if (!proto_is_vlan(h_proto)) */
-	/* 		break; */
-
-	/* 	if (vlh + 1 > data_end) */
-	/* 		break; */
-
-	/* 	h_proto = vlh->h_vlan_encapsulated_proto; */
-	/* 	vlh++; */
-	/* } */
-
-	nh->pos = vlh;
-	return h_proto; /* network-byte-order */
-}
-
-// the original function from <xdp/parsing_helpers.h> copied for debugging purposes
-static __always_inline int parse_iphdr_own(struct hdr_cursor *nh,
-				       void *data_end,
-				       struct iphdr **iphdr)
-{
-	struct iphdr *iph = nh->pos;
-	int hdrsize;
-
-	if (iph + 1 > data_end)
-		return -1;
-
-	hdrsize = iph->ihl * 4;
-
-	/* Variable-length IPv4 header, need to use byte-based arithmetic */
-	/* if (nh->pos + hdrsize > data_end) */ // BUG: causes errors on loading
-	/* 	return -1; */
-
-    (void) hdrsize;
-	/* nh->pos += hdrsize; */ // BUG: causes errors on loading
-	*iphdr = iph;
-
-	return iph->protocol;
-}
-
 SEC("xdp")
-int xdp_prog_simple(struct xdp_md *ctx) {
+int bc_pqp_xdp(struct xdp_md* ctx) {
     bpf_trace_printk("BC-PQP program called", 22);
 
-    void *data = (void *)(long)ctx->data;
+	void *data = (void *)(long)ctx->data;
     void *data_end = (void *)(long)ctx->data_end;
 	struct hdr_cursor nh;
     nh.pos = data;
 
-    bpf_trace_printk("Set data pointers", 18);
-
-	struct ethhdr *eth_header;
-	struct iphdr *ip_header;
-	/* struct ipv6hdr *ipv6_header; */
-	/* struct icmphdr_common *icmp_header; */
-
-    bpf_trace_printk("Parsing ETH...", 15);
-    int eth_type = parse_ethhdr_own(&nh, data_end, &eth_header);
+    struct ethhdr *eth_header;
+	int eth_type = parse_ethhdr(&nh, data_end, &eth_header);
     eth_type = bpf_ntohs(eth_type);
-    bpf_trace_printk("Parsed ETH... 0x%04x", 21, eth_type);
-    int ip_type;
-    if (eth_type == ETH_P_IP) {
-        ip_type = parse_iphdr_own(&nh, data_end, &ip_header);
-        if (ip_type != IPPROTO_ICMP) {
-            bpf_trace_printk("Passing IP packet with non-ICMP protocol", 41);
-            return XDP_PASS;
-        }
-    } else {
-        bpf_trace_printk("Passing non-IP packet", 22);
-        return XDP_PASS;
-    }
-    
-    // TODO: swap source and destination values
-    // TODO: fix checksums
+    bpf_trace_printk("ETH type: 0x%04x (0x%04x is IPv4, 0x%04x is IPv6)", 50, eth_type, ETH_P_IP, ETH_P_IPV6);
 
-    bpf_trace_printk("Reached end of function", 24);
+	struct iphdr *ipv4_header;
+	struct ipv6hdr *ipv6_header;
+	if (eth_type == ETH_P_IP) {
+		int ipv4_type = parse_iphdr(&nh, data_end, &ipv4_header);
+        bpf_trace_printk("IPv4 type: 0x%04x (0x%04x is the expected ICMP)", 48, ipv4_type, IPPROTO_ICMP);
+		if (ipv4_type != IPPROTO_ICMP) {
+			goto pass;
+        }
+	} else if (eth_type == ETH_P_IPV6) {
+		int ipv6_type = parse_ip6hdr(&nh, data_end, &ipv6_header);
+        bpf_trace_printk("IPv6 type: 0x%04x (0x%04x is the expected ICMPv6)", 50, ipv6_type, IPPROTO_ICMPV6);
+		if (ipv6_type != IPPROTO_ICMPV6) {
+			goto pass;
+        }
+	} else {
+		goto pass;
+	}
+
+    // swap source and destination IP address
+    __u16 echo_reply_type;
+	struct icmphdr_common *icmp_header;
+	int icmp_type = parse_icmphdr_common(&nh, data_end, &icmp_header);
+	if (eth_type == ETH_P_IP && icmp_type == ICMP_ECHO) {
+        __be32 tmp_saddr = ipv4_header->saddr;
+        ipv4_header->saddr = ipv4_header->daddr;
+        ipv4_header->daddr = tmp_saddr;
+		echo_reply_type = ICMP_ECHOREPLY;
+	} else if (eth_type == ETH_P_IPV6 && icmp_type == ICMPV6_ECHO_REQUEST) {
+        struct in6_addr tmp_saddr = ipv6_header->saddr;
+        ipv6_header->saddr = ipv6_header->daddr;
+        ipv6_header->daddr = tmp_saddr;
+		echo_reply_type = ICMPV6_ECHO_REPLY;
+	} else {
+		goto pass;
+	}
+
+    // swap source and destination MAC address
+	__u8 tmp_source[ETH_ALEN];
+	__builtin_memcpy(tmp_source, eth_header->h_source, ETH_ALEN);
+	__builtin_memcpy(eth_header->h_source, eth_header->h_dest, ETH_ALEN);
+	__builtin_memcpy(eth_header->h_dest, tmp_source, ETH_ALEN);
+
+    // compute new checksum
+    // (see https://github.com/xdp-project/xdp-tutorial/blob/6d3aa8191da499fae8bd4fd5aa89fefa3184274f/packet-solutions/xdp_prog_kern_03.c#L34)
+	__u16 old_checksum = icmp_header->cksum;
+	icmp_header->cksum = 0;
+	struct icmphdr_common old_icmp_header = *icmp_header;
+	icmp_header->type = echo_reply_type;
+    __u32 size = sizeof(struct icmphdr_common);
+	__u32 csum = bpf_csum_diff((__be32 *)&old_icmp_header, size, (__be32 *)icmp_header, size, ~old_checksum);
+	__u32 sum = (csum >> 16) + (csum & 0xffff);
+	sum += (sum >> 16);
+	icmp_header->cksum = ~sum;
+    bpf_trace_printk("Rewritten checksum 0x%04x -> 0x%04x", 36, old_checksum, icmp_header->cksum);
+
+    bpf_trace_printk("We are actually replying in XDP!", 33);
+	return XDP_TX;
+
+pass:
+    bpf_trace_printk("We are passing the packet to the kernel.", 41);
     return XDP_PASS;
 }
 
