a43770640f37b22c21bb0742571211ab5abab23b
Friedrich Tschirpke <friedrich.tschirpke@live.de>
2025-07-02 18:02:45 +0200

    chore: update load.sh with meaningful output messages

--- scripts/load.sh
+++ scripts/load.sh
@@ -6,9 +6,13 @@ else
     f=$1
 fi
 
+echo "Loading eBPF program $f"
 # timers need a userspace reference to work (thats why we use --pin-path)
 # see https://docs.ebpf.io/linux/helper-function/bpf_timer_init/
 xdp-loader load --pin-path /sys/fs/bpf/bc-pqp -m skb eth0 $f || exit 1
+
+echo "Resolving MAC addresses and writing them to eBPF map:"
+echo "If routes are not established, the MAC resolution might take a few seconds..."
 
 ping -c1 192.168.101.10 &> /dev/null
 ping -c1 192.168.102.10 &> /dev/null
