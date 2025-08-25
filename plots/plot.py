eb82fad92151ed6ce0b6674dbc9e59cbb18a2cf2
Friedrich Tschirpke <friedrich.tschirpke@live.de>
2025-08-25 14:57:15 +0200

    feat: plot everything

--- plots/plot.py
+++ plots/plot.py
@@ -328,7 +328,7 @@ def plot_iperf3_file(fig, ax, file: Path, options: IPerf3PlotOptions):
     ax.set_ylabel("Rate in Gbit/s")
 
 
-def plot_bursty_iperf3(file: Path, debug=False, bc_threshhold=True, rate=True, save=False, name="burst_iperf3", fixed_starts=None):
+def plot_bursty_iperf3(file: Path, debug=False, bc_threshhold=True, rate=True, save=False, name="burst_iperf3", fixed_starts=None, legend_pos=None):
     def val_func(byte_val, start, end):
         return (byte_val * 8 / 1024**3) / (end - start)
 
@@ -381,18 +381,13 @@ def plot_bursty_iperf3(file: Path, debug=False, bc_threshhold=True, rate=True, s
     accumulated_xs = []
     accumulated_ys = []
 
+    fs = 0
     if fixed_starts is None:
-        fs = None
-
         def check(i, fs):
             return abs(base_ys[i] - base_ys[max(i - 1, 1)]) < 0.02
     else:
-        fs = 0
-
         def check(i, fs):
             reached_start = i == fixed_starts[fs]
-            if reached_start:
-                fs += 1
             return not reached_start
 
     i = 1
@@ -403,6 +398,10 @@ def plot_bursty_iperf3(file: Path, debug=False, bc_threshhold=True, rate=True, s
             accumulated_xs.append(base_xs[i])
             accumulated_ys.append(base_ys[i])
             i += 1
+        if i >= len(base_ys):
+            print("ERROR: hit end")
+            break
+        fs += 1
 
         for j, y in enumerate(ys):
             if j == 0:
@@ -443,7 +442,10 @@ def plot_bursty_iperf3(file: Path, debug=False, bc_threshhold=True, rate=True, s
     ax.set_xlabel("Time after test start in seconds")
     ax.set_ylabel("Rate in Gbit/s")
 
-    ax.legend(loc="lower right")
+    if isinstance(legend_pos, tuple):
+        ax.legend(loc=legend_pos[0], bbox_to_anchor=legend_pos[1])
+    else:
+        ax.legend(loc=legend_pos)
 
     out_path = FINAL_PRESENTATION_DIR / f"{name}.pdf"
     save_or_show(fig, out_path, save)
@@ -681,16 +683,24 @@ def save_or_show(fig: plt.figure, path: Path, save_plot: bool):
 if __name__ == "__main__":
 
     save_plots = input("Press 'S' to save the plots instead of showing them: ").lower().startswith("s")
-    # plot_enforcement_server_file(DATA_DIR / "server-v2" / "rxq_8_flows_8.json", name="nonsharded_enforcement_8", show_linear=True, save=save_plots)
-    # plot_enforcement_server_file(DATA_DIR / "server-v2" / "rxq_8_flows_16.json", name="nonsharded_enforcement_16", show_linear=True, save=save_plots)
-    # plot_enforcement_server_file(DATA_DIR / "server-v2" / "rxq_8_flows_32.json", name="nonsharded_enforcement_32", show_linear=True, save=save_plots)
-    # plot_bursty_iperf3(DATA_DIR / "tcp" / "burst-server.json", name="nonsharded_tcp_burst", save=save_plots)
-    # plot_bursty_iperf3(DATA_DIR / "tcp" / "reno-burst-server.json", name="nonsharded_reno_tcp_burst", save=save_plots)
-    # # plot_flow_server_file(DATA_DIR / "server-v2" / "flow-data.json", name="nonshared_flow_exp", save=save_plots)
-    # plot_scale_server_file(DATA_DIR / "server-v2" / "scale-data.json", name="nonshared_scale_exp", save=save_plots)
-    # plot_scale_server_file(DATA_DIR / "server-v2" / "baseline_scaling.json", name="baseline_scale_exp", save=save_plots)
-    plot_bursty_iperf3(DATA_DIR / "burst" / "server.json", name="nonsharded_udp_burst", save=save_plots)
-    plot_bursty_iperf3(DATA_DIR / "burst" / "sharded-server.json", name="sharded_udp_burst", save=save_plots, fixed_starts=[41])
+    plot_enforcement_server_file(DATA_DIR / "server-v2" / "rxq_8_flows_8.json", name="nonsharded_enforcement_8", show_linear=True, save=save_plots)
+    plot_enforcement_server_file(DATA_DIR / "server-v2" / "rxq_8_flows_16.json", name="nonsharded_enforcement_16", show_linear=True, save=save_plots)
+    plot_enforcement_server_file(DATA_DIR / "server-v2" / "rxq_8_flows_32.json", name="nonsharded_enforcement_32", show_linear=True, save=save_plots)
+    plot_enforcement_server_file(DATA_DIR / "server-v2" / "rate_enforcement_phantom_queues_32.json", name="sharded_enforcement_32", show_linear=True, save=save_plots)
+    plot_bursty_iperf3(DATA_DIR / "tcp" / "burst-server.json", name="nonsharded_tcp_burst", save=save_plots,
+                       legend_pos="lower right")
+    plot_bursty_iperf3(DATA_DIR / "tcp" / "reno-burst-server.json", name="nonsharded_reno_tcp_burst", save=save_plots,
+                       legend_pos="lower right")
+    plot_flow_server_file(DATA_DIR / "server-v2" / "flow-data.json", name="nonshared_flow_exp", save=save_plots)
+    plot_scale_server_file(DATA_DIR / "server-v2" / "scale-data.json", name="nonshared_scale_exp", save=save_plots)
+    plot_scale_server_file(DATA_DIR / "server-v2" / "baseline_scaling.json", name="baseline_scale_exp", save=save_plots)
+    plot_scale_server_file(DATA_DIR / "server-v2" / "scaling_phantom_queues_32.json", name="sharded_scale_exp", save=save_plots)
+    plot_bursty_iperf3(DATA_DIR / "burst" / "server.json", name="nonsharded_udp_burst", save=save_plots,
+                       legend_pos="lower right")
+    plot_bursty_iperf3(DATA_DIR / "burst" / "sharded-server.json", name="sharded_udp_burst", save=save_plots,
+                       fixed_starts=[41], legend_pos="lower right")
+    plot_bursty_iperf3(DATA_DIR / "burst" / "sharded-server2.json", name="sharded_udp_burst2", save=save_plots,
+                       fixed_starts=[19, 58, 91, 125, 157], legend_pos=("upper right", (1, 0.95)))
 
     if input("Press ENTER to continue with \"exploration plots\" "):
         # do not show "exploration plots"
@@ -700,6 +710,11 @@ if __name__ == "__main__":
     options.iperf3_options.show_avg = False
     options.iperf3_options.show_enforced_rate = False
     options.legend = False
+
+    options.clear_files()
+    options.iperf3_files.append(DATA_DIR / "burst" / "sharded-client2.json")
+    options.iperf3_files.append(DATA_DIR / "burst" / "sharded-server2.json")
+    exploration_plots(options)
 
     options.clear_files()
     options.iperf3_files.append(DATA_DIR / "burst" / "sharded-client.json")
